691e23aeec4967f7095fdcb9599eae7f54be9ab6
Alexander Ushakov <alexander.n.ushakov@gmail.com>
2017-11-20 19:50:55 +0300

    LWDEV-4039 Required attributes added to model

--- src/Lykke.Service.ClientAssetRule/Models/RuleModel.cs
+++ src/Lykke.Service.ClientAssetRule/Models/RuleModel.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Lykke.Service.ClientAssetRule.Models
 {
     public class RuleModel
     {
+        [Required]
         public string Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string RegulationId { get; set; }
+
+        [Required]
         public List<string> AllowedAssetGroups { get; set; }
+
+        [Required]
         public List<string> DeclinedAssetGroups { get; set; }
     }
 }
