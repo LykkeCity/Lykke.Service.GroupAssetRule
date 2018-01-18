3205f2587bbe231f4a41bb18d8cfe394623462a6
Alexander Ushakov <alexander.n.ushakov@gmail.com>
2018-01-18 21:25:11 +0300

    LWDEV-5149 Check attribute added to asset service connection string

--- src/Lykke.Service.ClientAssetRule/Settings/Clients/AssetsServiceClient.cs
+++ src/Lykke.Service.ClientAssetRule/Settings/Clients/AssetsServiceClient.cs
@@ -1,7 +1,10 @@
-﻿namespace Lykke.Service.ClientAssetRule.Settings.Clients
+﻿using Lykke.SettingsReader.Attributes;
+
+namespace Lykke.Service.ClientAssetRule.Settings.Clients
 {
     public class AssetsServiceClientSettings
     {
+        [HttpCheck("/api/isalive")]
         public string ServiceUrl { get; set; }
     }
 }
